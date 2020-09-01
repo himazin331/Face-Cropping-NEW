86972d8257628da5257250e2a44b8637ca4d431e
himazin331 <sion20022013@gmail.com>
2020-09-02 01:15:01 +0900

    face cropping

--- face_cut_v2.py
+++ face_cut_v2.py
@@ -2,11 +2,12 @@
 import os
 import argparse as arg
 import sys
+import numpy as np
 from natsort import natsorted
 
 # 画像加工
-def face_cut(imgs_dir, result_out, img_size, label, HAAR_FILE):
-    
+def face_cut(imgs_dir, result_out, img_size, ch, label, HAAR_FILE):
+
     # Haar-Like特徴量Cascade型分類器の読み込み
     cascade = cv2.CascadeClassifier(HAAR_FILE)
     
@@ -15,15 +16,17 @@ def face_cut(imgs_dir, result_out, img_size, label, HAAR_FILE):
 
         print("画像データ:{}".format(img_name))
         
-        # jpg形式のみ
+        # 対応フォーマット -> JPEG, PNG, BMP
         _, ext = os.path.splitext(img_name)
-        if ext.lower() == '.jpg':
+        if ext.lower() in ['.jpg', '.png', '.bmp']:
             
-            img_path = os.path.join(imgs_dir, img_name) # ファイルパスの結合
-            img = cv2.imread(img_path)  # データ読み込み
+            img = cv2.imread(img_name)  # データ読み込み
             
-            img_g = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY) # グレースケールに変換
-            face = cascade.detectMultiScale(img_g)  # 顔を検出
+            # チャンネル数が１であれば
+            if ch == 1:
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # グレースケール変換
+
+            face = cascade.detectMultiScale(img)  # 顔を検出
     
             # 顔が検出されなかったら
             if len(face) == 0:
@@ -31,11 +34,16 @@ def face_cut(imgs_dir, result_out, img_size, label, HAAR_FILE):
                 print("Face not found.")
                 
             else:
+                for n in range(len(face)):
                 
-                for x,y,w,h in face:
+                    # X,Y座標、幅/高さ取得
+                    x = face[n][0]
+                    y = face[n][1]
+                    w = face[n][2]
+                    h = face[n][3]
 
                     # 顔の切り取り
-                    face_cut = img_g[y:y+h, x:x+w] 
+                    face_cut = img[y:y+h, x:x+w] 
                     # リサイズ
                     face_img = cv2.resize(face_cut, (img_size, img_size))
         
@@ -44,30 +52,29 @@ def face_cut(imgs_dir, result_out, img_size, label, HAAR_FILE):
                     cv2.imwrite(os.path.join(result_out + result_img_name), face_img)
                     label += 1
             
-                print("Processing success!!")
-                
+                    # 表示
+                    print("Processing success!!")
+
         else:
             print("Unsupported file extension")
     
 def main():
-    
-    # プログラム情報
-    print("Face image cropping Program ver.6")
-    print("Last update date:    2020/03/12 (Stop development)\n")
 
     # コマンドラインオプション作成
     parser = arg.ArgumentParser(description='Face image cropping')
-    parser.add_argument('--imgs_dir', '-d', type=str, default="test",
+    parser.add_argument('--imgs_dir', '-d', type=str, default=None,
                         help='画像フォルダパス(未指定ならエラー)')
     parser.add_argument('--out', '-o', type=str, 
                         default=os.path.dirname(os.path.abspath(__file__))+'/result_crop'.replace('/', os.sep),
                         help='加工後データの保存先(デフォルト値=./reslut_crop)')
     parser.add_argument('--img_size', '-s', type=int, default=32,
                         help='リサイズ(NxNならN,デフォルト値=32)')
+    parser.add_argument('--ch', '-ch', type=int, default=1,
+                        help='チャンネル数(デフォルト値=1)')
     parser.add_argument('--label', '-l', type=int, default=1,
                         help='dataN.jpgのNの初期値(デフォルト値=1)')
-    parser.add_argument('--haar_file', '-c', type=str, default=os.path.dirname(os.path.abspath(__file__))+'/haarcascade_frontalface_alt.xml'.replace('/', os.sep),
-                        help='haar-Cascadeのパス指定(デフォルト値=./haarcascade_frontalface_alt.xml)')
+    parser.add_argument('--haar_file', '-c', type=str, default=os.path.dirname(os.path.abspath(__file__))+'/haar_cascade.xml'.replace('/', os.sep),
+                        help='haar-Cascadeのパス指定(デフォルト値=./haar_cascade.xml)')
     args = parser.parse_args()
 
     # 画像フォルダ未指定時->例外
@@ -88,15 +95,16 @@ def main():
     print("# Images folder: {}".format(os.path.abspath(args.imgs_dir)))
     print("# Output folder: {}".format(args.out))
     print("# Images size: {}".format(args.img_size))
+    print("# Channel: {}".format(args.ch))
     print("# Start index: {}".format(args.label))
     print("# Haar-cascade: {}".format(args.haar_file))
     print("===========================\n")
-
+    
     # 出力フォルダの作成(フォルダが存在する場合は作成しない)
     os.makedirs(args.out, exist_ok=True)
-    
+
     # 加工
-    face_cut(args.imgs_dir, args.out, args.img_size, args.label, args.haar_file)
+    face_cut(args.imgs_dir, args.out, args.img_size, args.ch, args.label, args.haar_file)
     print("")
     
 if __name__ == '__main__':
